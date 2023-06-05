2b16edb35ee4d8b263f4abba251b31510c5b0758
Artyom <ardurkin14@gmail.com>
2023-06-05 20:25:19 +0300

    add new features

--- src/routes/project/apps/1/+layout.svelte
+++ src/routes/project/apps/1/+layout.svelte
@@ -1,12 +1,11 @@
-<div class='container'>
-    <a href='/project'>Домой</a>
-    <slot/>
-  </div>
+<div class="container">
+  <slot />
+</div>
   
-  <style>
-    .container {
-      margin: 50px auto;
-      max-width: 800px;
-      padding: 0 20px;
-    }
-  </style>
+<style>
+  .container {
+    margin: 50px auto;
+    max-width: 800px;
+    padding: 0 20px;
+  }
+</style>
--- src/routes/project/apps/1/+page.svelte
+++ src/routes/project/apps/1/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Icon from '../../../../components/Icon.svelte';
     let newItem = '';
     let todoList = [];
     function add() {
@@ -13,17 +14,41 @@
     newItem = "";
     }
     }
+    function remove(index) {
+    todoList.splice(index, 1);
+    todoList = todoList;
+    }
+    function complete(index) {
+    todoList[index].completed = !todoList[index].completed;
+    }
 </script>
+
+<h1>My to-do list</h1>
 
 <main>
     <form on:submit|preventDefault={add}>
         <input bind:value={newItem} placeholder="Enter to-do" />
         <button class="add-todo" on:click={add}><span>+</span></button>
       </form>
+      <div class="todos">
+        {#each todoList as item, index}
+        <div class="todo" class:completed={item.completed}>
+          <span class="todo__text">{item.task}</span>
+          <div class="todo__buttons">
+            <button class="complete" on:click={() => complete(index)}>
+              <Icon name="check-mark" />
+            </button>
+            <button class="delete" on:click={() => remove(index)}>
+              <Icon name="delete" />
+            </button>
+          </div>
+        </div>
+        {/each}
+      </div>
 </main>
 
 <style>
-    main {
+  main {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -31,16 +56,16 @@
     padding: 5vmin;
     box-sizing: border-box;
     background: antiquewhite;
-    }
-    form {
+  }
+  form {
     width: 100%;
     max-width: 500px;
     display: flex;
     align-items: center;
     margin-bottom: 1rem;
-    }
+  }
 
-    input {
+  input {
     flex-grow: 1;
     width: 0;
     border: none;
@@ -50,5 +75,59 @@
     font-size: 1.2rem;
     margin: 0;
     outline: none;
-    }
+  }
+  .todos {
+    width: 100%;
+    max-width: 500px;
+  }
+  .todo {
+    display: flex;
+    padding: 20px;
+    border-radius: 20px;
+    box-shadow: 0 0 15px rgb(0 0 0 / 20%);
+    background-color: hsla(0, 0%, 100%, 0.2);
+    margin-top: 1rem;
+    font-size: 1.2rem;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .todo__buttons {
+    display: flex;
+    align-items: center;
+    margin-left: 1rem;
+  }
+  .todo button {
+    width: 32px;
+    height: 32px;
+    padding: 4px;
+    margin: 0;
+    flex-shrink: 0;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 1.5rem;
+    margin: 2em 0;
+  }
+  button {
+    background-color: transparent;
+    border: none;
+  }
+  button.delete, button.delete:hover {
+    color: brown;
+    transition: color 100ms ease-out;
+  }
+  button.complete, button.complete:hover {
+    color: cadetblue;
+    transition: color 100ms ease-out;
+  }
+  .todo.completed {
+    color: slategray;
+  }
+  .todo.completed .todo__text {
+    text-decoration: line-through;
+  }
+  .todo.completed button {
+    color: silver;
+  }
 </style>
